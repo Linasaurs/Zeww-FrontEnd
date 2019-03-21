9861c7471428644e66d2e42a21ceff22723a0d78
Ziad Ali <ziadalikhalifa@gmail.com>
2019-03-21 12:39:02 +0200

    Made a test modal

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import CreateWorkspace from './components/CreateWorkspaceLayout/CreateWorkspace';
 import NavBar from './components/NavBar/NavBar';
+import ModalLayout from './components/ModalLayout/ModalLayout';
 
 class App extends Component {
   render() {
     return (
       <div className="App"> 
-      
+        <ModalLayout/>
       </div>
     );
   }
