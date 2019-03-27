d49160cb466074ea5b840ff297b98fa68834f661
Ziad Ali <ziadalikhalifa@gmail.com>
2019-03-27 17:22:30 +0200

    Fixed App.js to go to routing again

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
 import Routing from "./Routing/Routing";
-import CreateNewChannel from "./components/CreateNewChannelModal/CreateNewChannel";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <CreateNewChannel workspaceId="1" />
+                <Routing />
             </div>
         );
     }
