5189befe2f5e82d18ac14579399faac7d9d745c2
Ziad <ziadalikhalifa@gmail.com>
2019-03-31 13:42:00 +0200

    Fixed App.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,8 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <Routing /> 
-               
-            </div> 
+                <Routing />
+            </div>
         );
     }
 }
