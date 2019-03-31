32df58d7ad0cbe58ca5e0426dcd14c23b10a3b86
Ziad <ziadalikhalifa@gmail.com>
2019-03-31 09:00:42 +0200

    Fixed App.js to avoid conflicting master

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
 import Routing from "./Routing/Routing";
-import OmniSearch from "./components/OmniSearch/OmniSearch";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <OmniSearch workspaceId={12} />
+                <Routing />
             </div>
         );
     }
